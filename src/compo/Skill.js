f5d02e21a403a9bc21d7ae441607278b8dbbfd45
manickavasagar.n <manickavasagar.namasivayam@data-aces.com>
2024-02-28 00:28:48 +0530

    vue js added

--- src/compo/Skill.js
+++ src/compo/Skill.js
@@ -22,6 +22,10 @@ function Skill() {
                     <div class="progress" role="progressbar" aria-label="Animated striped example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
                         <div class="progress-bar progress-bar-striped progress-bar-animated" style={{width: '70%'}}>70%</div>
                     </div>
+                    <h6>vue JS</h6>
+                    <div class="progress" role="progressbar" aria-label="Animated striped example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated" style={{width: '60%'}}>60%</div>
+                    </div>
                     <h6>PHP</h6>
                     <div class="progress" role="progressbar" aria-label="Animated striped example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
                         <div class="progress-bar progress-bar-striped progress-bar-animated" style={{width: '85%'}}>85%</div>
